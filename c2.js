30cdefc43d5d302d76baecde2c80dac6c7a157a6
rajpal2 <109793235+rajpal2@users.noreply.github.com>
2022-08-05 14:39:40 +0530

    Add files via upload

--- c2.js
+++ c2.js
@@ -2,19 +2,28 @@ var express = require('express');
 var cookieParser = require('cookie-parser'); 
 var app = express(); 
 app.use(cookieParser()); 
+
+
 app.get('/cookieset',function(req, res){ 
-res.cookie('course_name', 'BCA'); 
-res.cookie('semester', 5); 
-res.cookie('coordinator', 'Ms. Krishna Bhati'); 
-res.cookie('university', 'MSU Baroda'); 
-res.status(200).send('Cookie is set'); 
+    res.cookie('course_name', 'BCA'); 
+    res.cookie('semester', 5); 
+    res.cookie('coordinator', 'Ms. Krishna Bhati'); 
+    res.cookie('university', 'MSU Baroda'); 
+    res.status(200).send('Cookie is set'); 
 }); 
+
+
 app.get('/cookieget', function(req, res) { 
-res.status(200).send(req.cookies); 
+    res.status(200).send(req.cookies); 
 }); 
+
+
 app.get('/', function (req, res) { 
 res.status(200).send('Welcome to world of Cookie!'); 
 }); 
+
+
+
 var server = app.listen(8000,"127.0.0.1", function () { 
 var host = server.address().address; 
 var port = server.address().port; 
